e6bd5730475de5151f9a82d879147cee5568966b
oscarRickovic <Med710scarrickovic@gmail.com>
2025-03-14 02:12:55 +0100

    add complex numbers version 2

--- src/main/java/visitor/Visitor.java
+++ src/main/java/visitor/Visitor.java
@@ -1,6 +1,7 @@
 package visitor;
 
 import calculator.MyNumber;
+import calculator.MyComplexNumber;
 import calculator.Operation;
 
 /**
@@ -14,10 +15,18 @@ public abstract class Visitor {
      * @param n The number being visited
      */
     public abstract void visit(MyNumber n);
+    
+    /**
+     * The Visitor can traverse a complex number (a subtype of Expression)
+     *
+     * @param n The complex number being visited
+     */
+    public abstract void visit(MyComplexNumber n);
 
     /**
      * The Visitor can traverse an operation (a subtype of Expression)
      *
      * @param o The operation being visited
-     */   public abstract void visit(Operation o);
+     */
+    public abstract void visit(Operation o);
 }
